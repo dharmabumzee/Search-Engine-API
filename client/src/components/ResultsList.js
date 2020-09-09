038e9f2b90db8fd9c7c677e7302f175fbe62f596
lukabelani <luka.belani@gmail.com>
2020-09-09 15:19:28 +0200

    add regex variables and extract title and url

--- client/src/components/ResultsList.js
+++ client/src/components/ResultsList.js
@@ -1,19 +1,28 @@
 import React from "react";
 
 const ResultsList = ({ results }) => {
+  const extractUrl = /.*href="(.*)".*/; // .replace
+
+  const extractContent = /<a[^>]*>.*<\/a>/gm; // .replace
+
+  const extractTitle = /<a [^>]+>([^<]+)<\/a>/; // .match
+
   return (
     <div className="results">
-      {results.map((result, index) => {
-        if (result.FirstURL)
+      {results &&
+        results.map((result, index) => {
           return (
-            <a href={result.FirstURL} className="search-results" key={index}>
-              <li
-                className="animate__animated animate__fadeIn ui list item"
-                dangerouslySetInnerHTML={{ __html: result.Result }}
-              />
+            <a
+              href={result.replace(extractUrl, "$1")}
+              className="search-results"
+              key={index}
+            >
+              <li className="animate__animated animate__fadeIn ui list item">
+                {result.match(extractTitle)[1]}
+              </li>
             </a>
           );
-      })}
+        })}
     </div>
   );
 };
