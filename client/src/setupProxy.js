44eec874ad5ae0f11033cc52adc84cc709047f58
lukabelani <luka.belani@gmail.com>
2020-09-16 01:34:36 +0200

    fix safari cors issue, chrome works fine

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,17 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const morgan = require("morgan");
 
 module.exports = function (app) {
   app.use(
-    ["/api", "auth/google"],
+    "/api",
     createProxyMiddleware({
       target: "http://localhost:5001",
       changeOrigin: true,
+      pathRewrite: {
+        "^api": "/api/v1",
+      },
     })
   );
+
+  app.use(morgan("combined"));
 };
